d5c4663f88ecee0e0f0d617eb2b04b84dd78b898
Daylen Jones <jonesday001@warren.k12.in.us>
2025-03-07 12:50:42 -0500

    ud

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Main {
+import java.util.function.BiFunction;
+
+@FunctionalInterface
+interface GCDCalculator {
+    int compute(int a, int b);
+}
+
+public class CustomGCD {
+    public static void main(String[] args) {
+        // Lambda expression to compute GCD
+        GCDCalculator gcd = (a, b) -> {
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return Math.abs(a); // Ensure non-negative GCD
+        };
+
+        // Test cases
+        int num1 = 56, num2 = 98;
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd.compute(num1, num2));
+
+        num1 = 48; num2 = 18;
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd.compute(num1, num2));
+    }
 }
